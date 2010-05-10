6ecbedbbf22ba0455936310d57b040bad6b0118d
Raony Guimaraes <raonyguimaraes@gmail.com>
2010-05-10 16:21:13 -0300

    Scripts Developed to parse SNPs

---
+++ src/scripts/insert_snps.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+#get a list of SNPs and insert at the database
+
+import sys
+import MySQLdb
+import os
+
+#connect to the database
+conn = MySQLdb.connect (host = "localhost",
+			user = "root",
+			passwd = "Epkghi59",
+			db = "biotorch")
+cursor = conn.cursor()
+
+
+#for arg in sys.argv: 
+filename = sys.argv[1]
+
+f = open(filename, "r")
+text = f.read()
+snps = text.split(',')
+for x in snps:
+   sql = "INSERT INTO %s (snpid) VALUES ('%s')" % ('snpedia', x) 
+   cursor.execute(sql)
+    #print x+'\n'
+    
+#insert SNPs at the database
+
+
+cursor.close ()
+conn.commit ()
+conn.close ()
